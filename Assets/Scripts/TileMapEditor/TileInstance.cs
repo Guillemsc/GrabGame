81c58fcf757819f4ae9210c47289833f3ddff80a
Guillemsc <gsunyercaldu@gmail.com>
2019-01-22 16:43:07 +0100

    Working on custom tile editor

---
+++ Assets/Scripts/TileMapEditor/TileInstance.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileInstance : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Sprite GetSprite()
+    {
+        Sprite ret = null;
+
+        if (sr != null)
+            ret = sr.sprite;
+
+        return ret;
+    }
+
+    [SerializeField]
+    private SpriteRenderer sr = null;
+
+    private Vector2Int grid_pos = Vector2Int.zero;
+}
